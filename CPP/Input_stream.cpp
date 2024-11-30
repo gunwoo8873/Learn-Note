c13c0221433cb13a30d5965f19d11b8eafabc787
gunwoo <gunwoo8873@outlook.kr>
2024-11-30 16:51:47 +0900

    Fix : Comment Note

--- CPP/Input_stream.cpp
+++ CPP/Input_stream.cpp
@@ -6,7 +6,7 @@ int main()
 {
   int input_number;
 
-  cout << "Enter the your Number Between 1 and 100 : ";  
+  cout << "Enter the your Number Between 1 and 100 : ";
 
   cin >> input_number;
 
@@ -14,4 +14,4 @@ int main()
 
   return 0;
 }
-// Note : cin은 Keyboard의 Input을 받아 값을 저장한다. 13 Line에서 cin의 값을 반환하여  << String << Input << 순으로 메모리 할당 및 해제 진행
+// Note : cin은 Keyboard의 Input값을 받아 값을 저장한다. 13 Line에서 cin의 값을 반환하여 << String << Input << 순으로 메모리 할당 및 해제 진행
