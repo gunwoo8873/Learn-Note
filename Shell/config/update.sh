b350ebb005a894bb6edbd5c32f900be69c259cf1
gunwoo <gunwoo8873@outlook.kr>
2024-12-11 19:53:35 +0900

    Shell

--- Shell/config/update.sh
+++ Shell/config/update.sh
@@ -2,6 +2,29 @@
 
 set -e
 
+function Selected_Version()
+{
+  options=("Ubuntu" "REHL" "Back")
+  select OPTIONS in "${options[@]}" do
+    case $OPTIONS in
+      Ubuntu);;
+      REHL);;
+      Back);;
+    esac
+  done
+}
+
+function Update_Version_Ubuntu()
+{
+  source ./bin/ubuntu_update.sh
+}
+
+function Update_Version_REHL()
+{
+  source ./bin/rehl_update.sh
+}
+
+
 function update_menu()
 {
   options=("Update" "Exit")
