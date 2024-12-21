756aba83c04b47ac4dda3165fd855d65b1688250
Gunwoo <gunwoo8873@outlook.kr>
2024-12-21 15:20:26 +0900

    Rename example rust files

--- Rust/src/bin/Thread.rs
+++ Rust/src/bin/Thread.rs
@@ -1,33 +1,39 @@
-use std::thread;
+use std::{
+  thread::{spawn, park, current, scope, sleep},
+  sync::{Arc, Condvar},
+  rc::Rc,
+};
 
 fn main()
 {
   println!("Main Thread"); // ThreadID N.1
 
-  thread::spawn(output); // ThreadID N.2
-  thread::spawn(output); // ThreadID N.3
+  spawn(output); // ThreadID N.2
+  spawn(output); // ThreadID N.3
 
   join();
   closure();
-  scope();
+  thread_scope();
   parking();
-  condition()
+  condition();
+  counting();
+  arc();
 }
 fn output()
 {
   println!("Another Thread");
 
-  let id = thread::current().id(); // This lib to Current ThreadID Checking Variable
+  let id = current().id(); // This lib to Current ThreadID Checking Variable
 
   println!("Thread id : {:?} ", id); // Thread ID Output
 }
 
 fn join()
 {
-  let t1 = thread::spawn(output); // ThreadID Add N.4
+  let t1 = spawn(output); // ThreadID Add N.4
   t1.join().unwrap();
 
-  let t2 = thread::spawn(output); // ThreadID Add N.5
+  let t2 = spawn(output); // ThreadID Add N.5
   t2.join().unwrap();
 }
 
@@ -36,7 +42,7 @@ fn closure()
   let num_vec_a = vec![1, 2, 3];
 
   // New Create Thread and Keyword Move use the Closure
-  thread::spawn(move || {
+  spawn(move || {
     for t in &num_vec_a
     {
       println!("{}", t);
@@ -47,7 +53,7 @@ fn closure()
 
   let num_vec_b = Vec::from_iter(0..=1000);
   
-    let t = thread::spawn(move || {
+    let t = spawn(move || {
         let len = num_vec_b.len();
         let sum = num_vec_b.into_iter().sum::<usize>();
         
@@ -58,11 +64,11 @@ fn closure()
     println!("{}", average);
 }
 
-fn scope()
+fn thread_scope()
 {
   let num_vec_a = vec![1, 2, 3];
 
-  thread::scope(|s| {
+  scope(|s| {
     s.spawn(|| {
       println!("length : {}", num_vec_a.len());
     });
@@ -81,7 +87,7 @@ use std::{sync::Mutex, time::Duration, collections::VecDeque};
 fn parking()
 {
   let queue = Mutex::new(VecDeque::new());
-  thread::scope(|s| {
+  scope(|s| {
     let t = s.spawn(|| loop {
       let item = queue.lock().unwrap().pop_front();
       
@@ -91,7 +97,7 @@ fn parking()
       }
       else
       {
-        thread::park(); // Fake Function return to Thread
+        park(); // Fake Function return to Thread
       }
     });
     
@@ -99,19 +105,17 @@ fn parking()
     {
       queue.lock().unwrap().push_back(i);
       t.thread().unpark();
-      thread::sleep(Duration::from_millis(100));
+      sleep(Duration::from_millis(100));
     }
   });
 }
-
-use std::sync::Condvar;
 
 fn condition()
 {
   let queue = Mutex::new(VecDeque::new());
   let not_empty = Condvar::new();
 
-  thread::scope(|s| {
+  scope(|s| {
     s.spawn(|| loop {
       let mut queue = queue.lock().unwrap();
       let item = loop
@@ -134,7 +138,33 @@ fn condition()
     {
       queue.lock().unwrap().push_back(i);
       not_empty.notify_one();
-      thread::sleep(Duration::from_millis(100));
+      sleep(Duration::from_millis(100));
     }
   });
 }
+
+fn counting()
+{
+    let a = Rc::new([1, 2, 3]);
+    let b = a.clone();       // a = [1,2,3]을 복사
+
+    assert_eq!(a.as_ptr(), b.as_ptr()); // a와 b의 raw pointer를 같은지 비교하고, 같은 메모리를 가리킨다.
+}
+// Note : as_ptr는 if의 == 연산자와 비슷한 성격을 가지고 있다. 같은 위치를 가리키고 있는지 확인하여,
+//        메모리 주소를 가리키는 raw pointer를 얻는다. [Waring : 메모리의 안전성을 보장받지 못하는 코드다. = unsafe]
+//        assert_eq!는 두 개의 값이 같은지 확인하여, 수행을 이어서 하지만 다르다면 즉시 실행을 중단하고
+//        Panic이 발생한다. [즉 오류가 발생하여 오류에 대한 MSG를 출력]
+
+fn arc()
+{
+    let a = Arc::new([1, 2, 3]);
+    let b = a.clone();
+
+    spawn(move || dbg!{a});
+    spawn(move || dbg!(b));
+}
+// Note : Send : 타입의 값과 소요권을 다른 Thread에 이전을 하고 싶다면 Send Trait를 구현하면 된다.
+//        Sync : 타입의 값을 여러 Thread에 공유하고 싶다라면 Sync Trait로 구현하면 된다.
+//        bool과 str 같은 원시 타입들은 Send와 Sync를 모두 구현하고 있기 때문에 필요한 구조체 필드의
+//        타입에 따라서 구조체에 해당 트레이트가 자동으로 구현되는 것을 Auto Trait라고 칭한다.
+//        T 타입으로 인식을 하지만 실제로 해당 타입은 존재하지 않아 크기가 0인 메모리를 차지하지 않는다.
