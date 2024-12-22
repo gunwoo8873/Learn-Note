a3de88208aca409e335ddd7c0dff18f91eddbb62
Gunwoo <gunwoo8873@outlook.kr>
2024-12-22 17:04:21 +0900

    Update : Add test code and grap, read file

--- Rust/src/bin/Grap.rs
+++ Rust/src/bin/Grap.rs
@@ -1,13 +1,46 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
-  let atgs: Vec<String> = env::args().collect(); // Don't Not Adjective unicode do panic!
+  path_grap();
+  read_file();
+}
 
-  let query = &atgs[1];
-  let file_path = &atgs[2];
+struct Config {
+  query: String,
+  file_path: String,
+}
 
-  println!("Searching for {}", query);
-  println!("In file {}", file_path);
+// impl Config {
+//   fn new(args: &[String]) -> Config {
+//     let query = args[1].clone();
+//     let file_path = args[2].clone();
+
+//     Config {query, file_path}
+//   }
+// }
+
+fn parse_config(args: &[String]) -> Config {
+  let query = args[1].clone();
+  let file_path = args[2].clone();
+
+  Config {query, file_path}
+}
+
+fn path_grap() {
+  let args: Vec<String> = env::args().collect(); // Not Adjective unicode is panic!
+  let config = parse_config(&args);
+
+  println!("Searching for {}", config.query);
+  println!("In file {}", config.file_path);
+}
+
+fn read_file() {
+  let args: Vec<String> = env::args().collect();
+  let config = parse_config(&args);
+
+  let contents = fs::read_to_string(config.file_path).expect("msg");
+
+  println!("With text:\n{}", contents);
 }
 
 #[cfg(test)]
@@ -15,7 +48,14 @@ mod tests {
   use super::*;
 
   #[test]
-  fn test_main() {
-    main();
+  fn test_grap() {
+    path_grap();
+  }
+
+  #[test]
+  fn test_read_file() {
+    read_file();
   }
 }
+
+// CMD : cargo run --bin grap -- test sample.txt
