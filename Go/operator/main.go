f35b1fb75a84f2621baae9c38c90d6a2be1dc04d
PITLANE <gunwoo8873@outlook.kr>
2025-02-24 21:50:31 +0900

    Update main.go

--- Go/operator/main.go
+++ Go/operator/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func tcpheader_example() {
 	var headerWords uint8 = 5         // Header length is 5 words
 	headerLen := headerWords * 32 / 8 // header len is 20bytes
 
@@ -29,4 +29,52 @@ func main() {
 	tcpSynFlag := (tcp_header[14] & 0x02) != 0 // Check if TCP Syn flag is set
 	parsedHeaderWords := tcp_header[13] >> 4
 	fmt.Printf("TCP SYN flag: %t\nParsed header length: %d\n", tcpSynFlag, parsedHeaderWords)
+}
+
+func arithmetic() {
+	var a, b int = 15, 40
+
+	sum := a + b
+	diff := a - b
+	product := a * b
+	quotient := a / b
+	remainder := a % b
+
+	fmt.Printf("Sum: %d, Difference: %d, Product: %d, Quotient: %d, Remainder: %d\n", sum, diff, product, quotient, remainder)
+}
+
+func logical() {
+	var a, b int = 4, 8
+
+	and := a & b
+	or := a | b
+	xor := a ^ b
+	not := ^a
+
+	fmt.Printf("And: %d, Or: %d, Xor: %d, Not: %d\n", and, or, xor, not)
+
+	equal := a == b
+	notEqual := a != b
+	lessThan := a < b
+	lessThanEqual := a <= b
+	greaterThan := a > b
+	greaterThanEqual := a >= b
+
+	fmt.Printf("Equal: %t, Not Equal: %t, Less Than: %t, Less Than Equal: %t, Greater Than: %t, Greater Than Equal: %t\n", equal, notEqual, lessThan, lessThanEqual, greaterThan, greaterThanEqual)
+}
+
+func comparison() {
+	var a, b int = 4, 2
+
+	left_shift := a << b
+	right_shift := a >> b
+
+	fmt.Printf("Left Shift: %d, Right Shift: %d\n", left_shift, right_shift)
+}
+
+func main() {
+	tcpheader_example()
+	arithmetic()
+	logical()
+	comparison()
 }
