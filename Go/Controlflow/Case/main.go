ac321db705fc9225684e465268cd66bc528d04b3
PITLANE <gunwoo8873@outlook.kr>
2025-03-08 12:51:01 +0900

    Update case code

---
+++ Go/Controlflow/Case/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	res, err := http.Get("https://google.com")
+	if err != nil {
+		log.Fatalf("Not error : %v", err)
+	}
+
+	switch {
+	case res.StatusCode == 600:
+		fmt.Println("Unknown")
+	case res.StatusCode == 500:
+		fmt.Println("Server Error")
+	case res.StatusCode == 400:
+		fmt.Println("Client Error")
+	case res.StatusCode == 300:
+		fmt.Println("Redirection")
+	case res.StatusCode == 200:
+		fmt.Println("Success")
+	case res.StatusCode == 100:
+		fmt.Println("Informational")
+	default:
+		fmt.Println("Incorrect")
+	}
+}
