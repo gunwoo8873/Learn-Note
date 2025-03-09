87f54f783f906e634ce6e168abdd74c7d583ca59
PITLANE <gunwoo8873@outlook.kr>
2025-03-09 21:23:00 +0900

    Frist build c#

---
+++ C#/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace CS {
+  class Program {
+    static void Main(string[] strings) {
+      Console.WriteLine("Hello, World!");
+    }
+  }
+}
