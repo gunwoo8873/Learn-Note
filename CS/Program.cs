3e212796c79e7124f7c41f7202ca21e66f85b0ea
PITLANE <gunwoo8873@outlook.kr>
2025-03-13 16:55:06 +0900

    Delete CS Files

--- CS/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-namespace CS {
-  class Program {
-    static void Main(string[] strings) {
-      Console.WriteLine("Hello, World!");
-    }
-  }
-}
