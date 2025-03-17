e009d151e6151dfef7a7ed33cf5adef35bc1afec
PITLANE <gunwoo8873@outlook.kr>
2025-03-18 00:53:07 +0900

    Update main.go

--- Go/operator/main.go
+++ Go/operator/main.go
@@ -49,10 +49,10 @@ func arithmetic() {
 func logical() {
 	var a, b int = 4, 8
 
-	and := a & b // 1 & 1 = 1, 0 & 1 = 0
-	or := a | b  // 1 | 1 = 1, 0 | 1 = 1, 0 | 0 = 0
+	and := a & b // OR a && b 1 & 1 = 1, 0 & 1 = 0
+	or := a | b  // OR a || b 1 | 1 = 1, 0 | 1 = 1, 0 | 0 = 0
 	xor := a ^ b // 1 ^ 1 = 0, 0 ^ 1 = 1, 0 ^ 0 = 0
-	not := ^a
+	not := ^a    // OR !a
 
 	fmt.Printf(
 		"And: %d, Or: %d, Xor: %d, Not: %d\n",
@@ -70,6 +70,14 @@ func logical() {
 		"Equal: %t\nNot Equal: %t\nLess Than: %t\nLess Than Equal: %t\nGreater Than: %t\nGreater Than Equal: %t\n",
 		equal, notEqual, lessThan, lessThanEqual, greaterThan, greaterThanEqual,
 	)
+
+	///////////////////////////////////////
+	// || 										[1순위 연산자]
+	// && 										[2순위 연산자]
+	// ==, !=, <, <=, >, >= 	[3순위 연산자]
+	// +, -, |, ^ 						[4순위 연산자]
+	// *, /, %, <<, >>, &, &^ [5순위 연산자]
+	///////////////////////////////////////
 }
 
 func comparison() {
