4815d237a471868b4db33f55c60d0a7bc83bd372
PITLANE <gunwoo8873@outlook.kr>
2025-03-18 00:12:54 +0900

    Update main.go

--- Go/fmt/main.go
+++ Go/fmt/main.go
@@ -32,9 +32,36 @@ func integerPrint() {
 	)
 }
 
-func mathPrint() {
-	float_value := 100.0
-	fmt.Printf("Math Print: %f\n", float_value)
+func floatPrint() {
+	float_value := 100.0000000
+	fmt.Printf("Float Print: %f\n", float_value)
+	fmt.Printf("Float Print: %g\n", float_value)
+}
+
+func stringPrint() {
+	str_value := "string value"
+	fmt.Printf("String Print: %s\n", str_value)
+}
+
+func memoryPrint() {
+	str_value := "string value"
+	fmt.Printf("Memory String Print: %p\n", &str_value)
+
+	int_value := 100
+	fmt.Printf("Memory Integer Print: %p\n", &int_value)
+}
+
+func whithPrint() {
+	int_value_a := 100
+	int_value_b := 150000.00000
+	fmt.Printf("Output whith size [5]Point Print: %5d, %5g\n", int_value_a, int_value_b)
+	fmt.Printf("Output whith left align [5]Point Print: %-5d, %-5g\n", int_value_a, int_value_b)
+	fmt.Printf("Output whith min size add 0 [5]Point Print: %05d, %05g\n", int_value_a, int_value_b)
+	fmt.Printf("Output Float [5]langth %8.3f\n", int_value_b)
+	// %5d = 5 digit,
+	// %-5d = 5 digit left align,
+	// %05d = 5 digit min size add 0 [00100, 150000]
+	// %N.Nf = [N] digit, [N] digit after point
 }
 
 func main() {
@@ -42,5 +69,8 @@ func main() {
 	dataTypePrint()
 	booleanPrint()
 	integerPrint()
-	mathPrint()
+	floatPrint()
+	stringPrint()
+	memoryPrint()
+	whithPrint()
 }
