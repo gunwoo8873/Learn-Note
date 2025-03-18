a047987db45885a1fbc9cf7cd086f833b8268751
PITLANE <gunwoo8873@outlook.kr>
2025-03-18 19:57:31 +0900

    Update main.go

--- Go/Controlflow/If/main.go
+++ Go/Controlflow/If/main.go
@@ -2,16 +2,46 @@ package main
 
 import "fmt"
 
-func main() {
-	var a = make([]int, 30)
+func arrLoop() {
+	a := make([]int, 10)
 
 	for i := range a {
 		a[i] = i + 1
 
-		if a[i] > 20 {
+		if a[i] > 5 {
 			fmt.Println(a) // A Slice value is limited to 20 [0..=20]
 			fmt.Println("Slice value is over 20 and process is exit")
 			break
 		}
 	}
+}
+
+func multipleIFLoop() {
+	a := make([]int, 10)
+
+	for i := range a {
+		a[i] = i + 1
+
+		if a[i] == 10 {
+			if a[i] == 3 {
+				fmt.Println(a[i])
+				continue
+			}
+
+			if a[i] == 6 {
+				fmt.Println(a[i])
+				continue
+			}
+
+			if a[i] == 9 {
+				fmt.Println(a[i])
+				break
+			}
+		}
+	}
+}
+
+func main() {
+	arrLoop()
+	multipleIFLoop()
 }
