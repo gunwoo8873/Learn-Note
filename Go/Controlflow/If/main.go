4052e410fa0d958166be5c3ee8e3717ef2f3f466
PITLANE <gunwoo8873@outlook.kr>
2025-03-19 13:59:25 +0900

    Update main.go

--- Go/Controlflow/If/main.go
+++ Go/Controlflow/If/main.go
@@ -16,32 +16,6 @@ func arrLoop() {
 	}
 }
 
-func multipleIFLoop() {
-	a := make([]int, 10)
-
-	for i := range a {
-		a[i] = i + 1
-
-		if a[i] == 10 {
-			if a[i] == 3 {
-				fmt.Println(a[i])
-				continue
-			}
-
-			if a[i] == 6 {
-				fmt.Println(a[i])
-				continue
-			}
-
-			if a[i] == 9 {
-				fmt.Println(a[i])
-				break
-			}
-		}
-	}
-}
-
 func main() {
 	arrLoop()
-	multipleIFLoop()
 }
