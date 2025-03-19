c0c895089bccc8144b0c3dd18c18823d29e912fe
PITLANE <gunwoo8873@outlook.kr>
2025-03-19 19:05:08 +0900

    Create main.go

---
+++ Go/Instance/main.go
@@ -0,0 +1,19 @@
+package main
+
+///////////////////////////////////////////////////
+// Instance //
+// 1. Instance는 메모리에 할당된 데이터의 실체
+// 2. Garbage Collector라는 메모리 청소 기능을 제공
+/////////////////////////////////////////////////
+
+func main() {
+	// var data Data       	  // Data type instance create
+	// var p *Data = &data	  // Clone address of data into p
+
+	// valueA := &Data{N, N} 			// & used initializ
+	// var valueB = new(Data) // new() used initializ
+
+	// valueA := &User{}
+	// valueA.Age = 30
+	// fmt.Println(valueA)
+}
