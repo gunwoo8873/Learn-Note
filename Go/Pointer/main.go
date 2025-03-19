e5866c47284779b734b6adc2a4c37331107984d2
PITLANE <gunwoo8873@outlook.kr>
2025-03-19 19:41:58 +0900

    Update main.go

--- Go/Pointer/main.go
+++ Go/Pointer/main.go
@@ -2,14 +2,25 @@ package main
 
 import "fmt"
 
-func pointer(iptr *int) {
-	*iptr = 0
+///////////////////////////////////////////////////////
+// Pointer
+// 1. Memory의 주소를 값으로 갖는 타입
+// 2. 메모리 복사를 줄여 반환값 없이 변숫값을 변경
+// 3. Pointer는 변숫값을 초기화하지 않으면 기본값은 nil
+///////////////////////////////////////////////////////
+
+func standardPointer() {
+	var a int = 500
+	var p *int // Pointer variable declaration
+
+	fmt.Printf("Value of a: %d, Address : %p\n", a, &a)
+
+	p = &a   // Clone address of a into p
+	*p = 100 // Change value of a
+
+	fmt.Printf("Value of p: %d, Address : %p\n", *p, p)
 }
 
 func main() {
-	i := 1
-
-	pointer(&i)
-	fmt.Println("value:", i)
-	fmt.Println("pointer:", &i)
+	standardPointer()
 }
