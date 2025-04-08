fd165197774f84f9cc35fdb8f879d018ac360fc5
PITLANE <gunwoo8873@outlook.kr>
2025-04-09 02:36:51 +0900

    Update HelloWorld.cs

--- CS/HelloWorld/HelloWorld.cs
+++ CS/HelloWorld/HelloWorld.cs
@@ -1,6 +1,7 @@
-﻿// You can class name change Optional
+﻿// Class name change is possible and this is developer's choice.
+// Class 이름은 변경이 가능하고 이는 개발자의 선택 사항입니다.
 class HelloWorld_Class {
-  // Need to Main method to run the program the don't change method name
+  // Need to Main method to run the program the don't change identifier[식별자] method name
   static void Main(string[] args) {
     Console.WriteLine("Hello, World!");
   }
