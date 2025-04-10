c5e8b1c61a23b6cbb4b35c11673b5ebee247c2da
PITLANE <gunwoo8873@outlook.kr>
2025-04-11 04:55:25 +0900

    Test StackandHeap memory area learn note

---
+++ CS/StackandHeap/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace StackNS {
+  public class StackClass {
+    int stackValue = 10; // Save in the stack memory area
+    
+    public static void PrintStackValue() {
+      Console.WriteLine("Stack Value: " + stackValue);
+    }
+  }
+}
+
+namespace HeapNS {
+  public class HeapClass {
+    int heapValue = new int(); // Save in the heap memory area
+
+    public static void PrintHeapValue() {
+      Console.WriteLine("Heap Value: " + heapValue);
+    }
+  }
+}
+
+class Compile {
+  static void Main(string[] args) {
+    StackNS.StackClass.PrintStackValue();
+    HeapNS.HeapClass.PrintHeapValue();
+  }
+}
