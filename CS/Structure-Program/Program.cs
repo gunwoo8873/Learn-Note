ed4f16210ebfed11975808bfab671f5cf3a5e544
PITLANE <gunwoo8873@outlook.kr>
2025-04-19 17:48:14 +0900

    Structure Program

---
+++ CS/Structure-Program/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RootNamespace {
+  public class PublicClass {
+    struct PublicStruct {}
+
+    interface PublicInterface {}
+  }
+
+  private class PrivateClass {
+    struct PrivateStruct {}
+
+    interface PrivateInterface {}
+  }
+
+  namespace SubNamespace {}
+
+  delegate int DelegateType(int a, int b);
+
+  class Compiler {
+    public static void Main(string[] args) {
+      Console.WriteLine("Hello, World!");
+    }
+  }
+}
