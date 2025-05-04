724059734cedf5a8c79aad390164900df324cbae
PITLANE <gunwoo8873@outlook.kr>
2025-05-04 11:53:27 +0900

    Update HelloWorld.cs

--- CS/HelloWorld/HelloWorld.cs
+++ CS/HelloWorld/HelloWorld.cs
@@ -1,15 +1,24 @@
 ﻿class HelloWorldCS {
-  // Warning : Don`t change the class name, it is a convention to use PascalCase for class names.
-  // Need to Main method to run the program the don't change identifier[식별자] method name
-  // Main method is the entry point of the program.
   static void Main(string[] args) {
     Console.WriteLine("Hello, World!");
     
-    const string mutable_value = "Mutable Value";
+    // const keyword is only one used for mutable values.
+    const string immutable_value = "Mutable Value";
+    Console.WriteLine(immutable_value);
+
+    // not used const keyword to then after can do update value
+    string mutable_value = "Immutable Value";
     Console.WriteLine(mutable_value);
 
-    string immutable_value = "Immutable Value";
-    Console.WriteLine(immutable_value);
+    // mutable value is changed to update the value.
+    mutable_value = "Changed Immutable Value";
+    Console.WriteLine(mutable_value);
   }
 }
-// Note : Class name change is possible and this is developer's choice.
+// Note :
+// Class name change is possible and this is developer's choice.
+
+// Warning :
+// Don`t change the class name, it is a convention to use PascalCase for class names. 
+// Need to Main method to run the program the don't change identifier[식별자] method name
+// Main method is the entry point of the program.
