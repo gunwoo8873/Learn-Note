c6e3f0cda8c5fdb752c41f3dda04fd1a37268fe4
PITLANE <gunwoo8873@outlook.kr>
2025-05-05 18:06:19 +0900

    Create Array.cs

---
+++ CS/Array/Array.cs
@@ -0,0 +1,11 @@
+﻿namespace InstanceArrNS {
+  public class ArrCS {
+    int [] products = new int[1000];
+  }
+}
+
+namespace NS {
+  class CS {
+    public static void Main(string[] args) {}
+  }
+}
