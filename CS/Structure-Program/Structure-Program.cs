1eccb83b219fd9e4a9950271aff9bd20364aa332
PITLANE <gunwoo8873@outlook.kr>
2025-05-05 15:52:11 +0900

    Update Structure-Program.cs

--- CS/Structure-Program/Structure-Program.cs
+++ CS/Structure-Program/Structure-Program.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 
-namespace RootNamespace {
-  ///////////// Public /////////////
-  // Public keyword is used to declare a class, struct, interface,
-  // or namespace that can be accessed from any other
-  // code in the same assembly or another assembly that references it.
-  public class PublicClass {
-    public struct PublicStruct {}
+public record PublicRecord {}
 
-    public interface PublicInterface {
-      // Method in the public interface
-      void Method();
+///////////// Public /////////////
+// Public keyword is used to declare a class, struct, interface,
+// or namespace that can be accessed from any other
+// code in the same assembly or another assembly that references it.
+public class PublicClass {
+    public struct Struct {}
+
+    public interface Interface {
+      // ...
+    }
+
+    public class SubClassA : Interface {
+      public void Method(int parameter) {
+        // ...
+      }
     }
   }
 
@@ -18,24 +24,20 @@ namespace RootNamespace {
   // Private keyword is not used to declare a class, struct, interface,
   // or namespace that can only be accessed within the same class or struct.
   // Private members are not accessible outside the class or struct they are declared in.
-  private class PrivateClass {
-    private struct PrivateStruct {}
+  // Note : Not use public keyword to default to private.
+private class PrivateClass {
+  struct PrivateStruct {}
 
-    private interface PrivateInterface {
-      // Method in the private interface
-      void Method();
-    }
+  interface PrivateInterface {
+    // ...
   }
+}
 
-  namespace SubNamespace {}
+delegate int DelegateType(int a, int b);
+// Delegate keyword is used to declare a delegate type.
+// A delegate is a type that represents references to methods with a specific parameter list and return type.
 
-  delegate int DelegateType(int a, int b);
-  // Delegate keyword is used to declare a delegate type.
-  // A delegate is a type that represents references to methods with a specific parameter list and return type.
-
-  class Compiler {
-    public static void Main(string[] args) {
-      RootNamespace obj = new PublicClass();
-    }
+class Compiler {
+  public static void Main(string[] args) {
   }
 }
