61988588f3e9af2d299b6ec6efcb6099c2758543
choigunwoo <gunwoo8873@outlook.kr>
2025-05-25 20:00:45 +0900

    Variable

---
+++ CS/Variable/Program.cs
@@ -0,0 +1,15 @@
+﻿// Variable name role
+// 1. can be specified using strings, numbers, or special characters.
+// 2. need to first words is string or special characters. = [variable, _variable]
+// 3. Case sensitive[구분] strings
+// 4. A declared[선언] variable can have values, and each value has one data type
+
+string strValue;
+strValue = "Small";
+
+Console.WriteLine(strValue);
+
+string StrValue;
+StrValue = "Big";
+
+Console.WriteLine(StrValue);
