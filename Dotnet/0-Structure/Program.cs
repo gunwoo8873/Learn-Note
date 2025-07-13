308b85b44bcb09c1864ee3ef5f024c882b8d92d7
choigunwoo <gunwoo8873@outlook.kr>
2025-07-13 19:57:47 +0900

    Fix Namespace in the field structer

--- Dotnet/0-Structure/Program.cs
+++ Dotnet/0-Structure/Program.cs
@@ -1,56 +1,50 @@
 ﻿using System;
 
-namespace NS
+namespace Public
 {
   // Use public keyword is you can using member
   public class PublicCS
   {
-    public static void Function(string[] args)
-    {
-      Console.WriteLine("Public Funtion Output");
-    }
-
+    //...
     public struct Struct
     {
-      // ...
+      //...
     }
 
     public interface Interface
     {
-      // ...
+      //...
     }
   }
+}
 
+namespace Private
+{
   // Not use public keyword is default private keyword in the outside cannot access
-  class PrivateCS
+  private class Menual_PrivateClass
   {
-    static void Function(string[] args)
+    //...
+    private struct Struct
     {
-      Console.WriteLine("Private Function Output");
-    }
-
-    struct Struct
-    {
-      // ...
-    }
-
-    interface Interface
-    {
-      // ...
+      //...
     }
   }
+}
 
-  namespace SubNS
-  {
-    
-  }
 
-  class Complier
+namespace Default_None_Public
+{
+  class Default_PricateClass
   {
-    public static void Main(string[] args)
+    //...
+    struct Default_Struct
     {
-      PublicCS.Function(args); // Can public class access
-      PrivateCS.Function(args); // Private class cannot access [Error Code : CS0122]
+      //...
+    }
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World");
     }
   }
 }
