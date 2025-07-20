04bcf7de547f511a878a58f1b70ddc8c5b0de470
choigunwoo <gunwoo8873@outlook.kr>
2025-07-21 04:57:42 +0900

    Fix Tutorial variable

---
+++ Go/Variable/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	//// "var" keyword is you can update value
+	var Integer int = 100
+	fmt.Println(Integer)
+
+	//// "const" keyword is you can't update value and only one use
+	const String string = "String"
+	fmt.Println(String)
+}
