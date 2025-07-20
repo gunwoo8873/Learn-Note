1ca4e8dae4a2b70cbc70bf32621eb21395ce88c0
choigunwoo <gunwoo8873@outlook.kr>
2025-07-20 17:27:37 +0900

    -

---
+++ Certification/SAA/Source/SDK-DotnetVer/Program.cs
@@ -0,0 +1,17 @@
+﻿// Dotnet use librarys
+using System;
+using System.Threading.Tasks;
+
+// Amazon API librarys
+using Amazon.Runtime;
+
+namespace SDK_S3Bucket
+{
+    public class S3_List
+    {
+        public static async Task(string[] args)
+        {
+            var ssoCreds = LoadSSOCredentials("default");
+        }
+    }
+}
