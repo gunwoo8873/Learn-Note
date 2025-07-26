f2178ff1724e8abe06f92869242a7a6959082e57
choigunwoo <gunwoo8873@outlook.kr>
2025-07-26 19:09:04 +0900

    Add float

--- Go/2-Datatype/main.go
+++ Go/2-Datatype/main.go
@@ -7,47 +7,47 @@ import (
 
 func integer() {
 	int8 := math.MaxInt8
-	fmt.Println(int8)
+	fmt.Println("Int 8 bit :", int8)
 
 	int16 := math.MaxInt16
-	fmt.Println(int16)
+	fmt.Println("Int 16 bit :", int16)
 
 	int32 := math.MaxInt32 // Or uint8 name to 'rune' is UTF-8 Char data output
-	fmt.Println(int32)
+	fmt.Println("Int 32 bit :", int32)
 
 	int64 := math.MaxInt64
-	fmt.Println(int64)
+	fmt.Println("Int 64 bit :", int64)
 }
 
 func uinteger() {
 	uint8 := math.MaxUint8 // Or uint8 name to 'byte' is 1 byte data output
-	fmt.Println(uint8)
+	fmt.Println("Uint 8 bit :", uint8)
 
 	uint16 := math.MaxUint16
-	fmt.Println(uint16)
+	fmt.Println("Uint 16 bit :", uint16)
 
 	uint32 := math.MaxUint32
-	fmt.Println(uint32)
+	fmt.Println("Uint 32 bit :", uint32)
 
 	//// Short keyword using for variable to Int64 is max value limit error then add 'var' keyword use
 	var uint64 uint64 = math.MaxUint64
-	fmt.Println(uint64)
+	fmt.Println("Uint 64 bit :", uint64)
 }
 
 func float() {
 	float32 := math.MaxFloat32
-	fmt.Println(float32)
+	fmt.Println("Float 32 bit :", float32)
 
 	float64 := math.MaxFloat64
-	fmt.Println(float64)
+	fmt.Println("Float 64 bit :", float64)
 }
 
 func boolean() {
 	bool := true
-	fmt.Println(bool)
+	fmt.Println("Boolean true :", bool)
 
 	bool = false
-	fmt.Println(bool)
+	fmt.Println("Boolean false :", bool)
 }
 
 func main() {
