9fa3da093d8633c38f1f6bf097042e6df79025c1
choigunwoo <gunwoo8873@outlook.kr>
2025-07-26 16:38:14 +0900

    Fix golang variable code

---
+++ Go/1-Variable/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func varKeyowrd() {
+	var v string = "This is variable"
+	fmt.Println(v)
+
+	v = "This is update after value"
+	fmt.Println(v)
+}
+
+func constKeyword() {
+	const v string = "This is constant"
+	fmt.Println(v)
+
+	//// constant is one variable to only one value
+	// v = "Error"
+	// fmt.Println(v)
+}
+
+func shortKeyword() {
+	v := "This is short variable"
+	fmt.Println(v)
+
+	//// Do not equle datatype can't use
+	// v := 10
+	// fmt.Println(v)
+
+	v = "This is update after short variable"
+	fmt.Println(v)
+}
+
+func main() {
+	varKeyowrd()
+	constKeyword()
+	shortKeyword()
+}
