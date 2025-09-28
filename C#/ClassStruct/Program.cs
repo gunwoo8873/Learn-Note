bda2d80a9cab564c88bc0081116f3374dde5b353
choigunwoo <gunwoo8873@outlook.kr>
2025-09-28 09:30:53 +0900

    -

---
+++ C#/ClassStruct/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+
+class StandardMethods
+{
+    static void NormalReturnMethod()
+    {
+        //...
+    }
+
+    static int IntegerReturnMethod()
+    {
+        //...
+    }
+}
+
+
+class AsyncMethods
+{
+    // 'string[]'는 매게 변수 타입
+    static async Task AsyncTaskMethod(string[] args)
+    {
+        return await Task.FromResult(args);
+    }
+
+    static async Task<int> IntegerAsyncTaskMethod()
+    {
+        return await Task.FromResult<int>(0);
+    }
+}
